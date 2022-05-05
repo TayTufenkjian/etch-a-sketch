5c964f872eb3fbfcc0cd63b43ba543478ca63012
TayTufenkjian <taytuf@gmail.com>
2022-05-05 17:44:43 -0400

    Calculate and set container dimensions
    To handle different window sizes, calculate the dimensions of
    the grid container based on the body width and height;
    then size the grid squares accordingly

--- script.js
+++ script.js
@@ -3,11 +3,33 @@ document.addEventListener('DOMContentLoaded', () => {
     createGrid(16);
 })
 
+
 function createGrid(num) {
-    // Get the grid container and set its dimensions
+    // Get the grid container, body, and header 
     const container = document.querySelector('#container');
-    container.style.gridTemplateColumns = `repeat(${num}, 45px)`;
-    container.style.gridTemplateRows = `repeat(${num},45px)`;
+    const body = document.querySelector('body');
+    const header = document.querySelector('header');
+
+    // If body width is greater than height, use the height to calculate container dimensions
+    // Otherwise, use the width to calculate container dimensions
+    // Add 10px of wiggle room
+    if (body.offsetWidth > body.offsetHeight) {
+        containerSize = body.offsetHeight - header.offsetHeight - 10;
+    } else {
+        containerSize = body.offsetWidth - 10;
+    }
+    container.style.height = `${containerSize}px`;
+    container.style.width = `${containerSize}px`;
+
+    // Set outer container width equal to container width for layout purposes
+    document.querySelector('#outer-container').style.width = `${containerSize}px`;
+
+    // Calculate the size of each square
+    let squareSize = containerSize / num;
+
+    // Set grid columns and rows using the square size
+    container.style.gridTemplateColumns = `repeat(${num}, ${squareSize}px)`;
+    container.style.gridTemplateRows = `repeat(${num}, ${squareSize}px)`;
 
     // Create the squares and add them to the container
     let numSquares = num * num;
@@ -20,4 +42,13 @@ function createGrid(num) {
     // Listen for hover on each square and apply color
     let squares = document.querySelectorAll('.square');
     squares.forEach(square => square.addEventListener('mouseover', () => square.classList.add('colored')));
+}
+
+
+function createCustomGrid() {
+    let size = prompt('Enter a number between 1 and 100 for your grid size.');
+    if (size > 0 && size <= 100) {
+        document.querySelector('#container').innerHTML = '';
+        createGrid(size);
+    }
 }
