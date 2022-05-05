c38338464718a7ff7486268c295c9af4216abdca
TayTufenkjian <taytuf@gmail.com>
2022-05-05 15:33:58 -0400

    Simplify element and variable names

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     // Create the initial 16x16 grid 
     createGrid(16);
 })
 
 function createGrid(num) {
     // Get the grid container and set its dimensions
-    const gridContainer = document.querySelector('#grid-container');
-    gridContainer.style.gridTemplateColumns = `repeat(${num}, 45px)`;
-    gridContainer.style.gridTemplateRows = `repeat(${num},45px)`;
+    const container = document.querySelector('#container');
+    container.style.gridTemplateColumns = `repeat(${num}, 45px)`;
+    container.style.gridTemplateRows = `repeat(${num},45px)`;
 
     // Create the squares and add them to the container
-    let squares = num * num;
-    for (i = 0; i < squares; i++) {
+    let numSquares = num * num;
+    for (i = 0; i < numSquares; i++) {
         let square = document.createElement('div');
-        square.classList.add('grid-item');
-        gridContainer.appendChild(square);
+        square.classList.add('square');
+        container.appendChild(square);
     }
 }
